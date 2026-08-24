f6a31f825fef7da8062e29c5e2004c7ab9789795
jayshreeganesh333 <jayshreeganesh333@gmail.com>
2026-08-24 20:29:33 +0530

    feat: Add database config
    Sets up SQLite and MySQL connection configurations.

---
+++ config/database.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Database Configuration (Laravel Style)
+ */
+
+return [
+    'default' => 'sqlite', // 'sqlite' or 'mysql'
+
+    'connections' => [
+        'sqlite' => [
+            'driver'   => 'sqlite',
+            'database' => (defined('LARAVEL_ROOT') ? LARAVEL_ROOT : dirname(__DIR__)) . '/database/database.sqlite',
+            'prefix'   => '',
+        ],
+
+        'mysql' => [
+            'driver'    => 'mysql',
+            'host'      => '127.0.0.1',
+            'port'      => 3306,
+            'database'  => 'laravel_crud',
+            'username'  => 'root',
+            'password'  => '',
+            'charset'   => 'utf8mb4',
+            'prefix'    => '',
+        ],
+    ],
+];
